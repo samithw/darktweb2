ff601bceddfa203e86e866085dec12bacc8b8cb2
Samith Wickramasekara <samithwik@gmail.com>
2025-12-18 18:17:22 +0000

    can you make it slightly bigger?

--- src/components/page/template-card.tsx
+++ src/components/page/template-card.tsx
@@ -17,8 +17,8 @@ type TemplateCardProps = {
 
 const WhatsAppIcon = () => (
     <svg
-        width="16"
-        height="16"
+        width="18"
+        height="18"
         viewBox="0 0 24 24"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
